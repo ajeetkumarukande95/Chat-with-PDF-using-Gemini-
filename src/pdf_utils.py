483c37d8703cf19d74507236c3405c5539802a8d
ajeetkumarukande95 <163981460+ajeetkumarukande95@users.noreply.github.com>
2024-08-21 00:13:42 +0530

    Create pdf_utils.py

---
+++ src/pdf_utils.py
@@ -0,0 +1,15 @@
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
